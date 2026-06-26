cce8344c3cc36f44bc31f542f21ba5e5e22e48e9
Deepak Kumar <Deepak11op11@gmail.com>
2026-06-27 00:21:08 +0530

    Add CORS middleware and update FastAPI API

--- MLOPS/API_IMP/First_Project/API/main.py
+++ MLOPS/API_IMP/First_Project/API/main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from API.schema import DeliveryInput
 from API.model import model, features
 import pandas as pd
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return {"message": "Food Delivery Time Prediction API"}
 
-
 @app.post("/predict")
 def predict(data: DeliveryInput):
-
     df = pd.DataFrame([data.model_dump()])
-
     df = pd.get_dummies(df)
-
     df = df.reindex(columns=features, fill_value=0)
 
     prediction = model.predict(df)
